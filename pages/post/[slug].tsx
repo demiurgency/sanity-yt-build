763ae4f8427c5eaabb157dbc960fef40adee413b
Anthony Greene <agreene@sidlee.com>
2022-07-03 17:37:30 -0400

    html form

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,7 +4,7 @@ import { Post } from '../../typings';
 import PortableText from 'react-portable-text';
 //import {PortableText} from '@portabletext/react'
 import { Children } from 'react';
-import { GetStaticProps } from 'next'
+import { GetStaticProps } from 'next';
 
 interface Props {
   post: Post;
@@ -38,7 +38,6 @@ function Post({ post }: Props) {
           </p>
         </div>
         <div className='mt-10'>
-         
           <PortableText
             className=''
             dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
@@ -61,9 +60,26 @@ function Post({ post }: Props) {
               ),
             }}
           />
-     
         </div>
       </article>
+      <hr className='max-w-lg my-5 mx-auto border border-yellow-500' />
+      <form className='flex flex-col p-5 my-10 max-w-2xl mx-auto mb-10'>
+        <h3 className='text-sm text-yellow-500'>Enjoyed the article?</h3>
+        <h4 className='text-3xl font-bold'>Leave a comment below</h4>
+        <hr className='py-3 mt-2' />
+        <label className='block mb-5'>
+          <span className='text-gray-700'>Name</span>
+          <input className='shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring' placeholder='John Appleseed0' type='text' />
+        </label>
+        <label  className='block mb-5'>
+          <span className='text-gray-700'>Email</span>
+          <input className='shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring' placeholder='John Appleseed0' type='text' />
+        </label>
+        <label className='block mb-5'>
+          <span className='text-gray-700'>Comment</span>
+          <textarea className='shadow border rounded py-2 px-3  form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring' placeholder='John Appleseed0' rows={8} />
+        </label>
+      </form>
     </main>
   );
 }
