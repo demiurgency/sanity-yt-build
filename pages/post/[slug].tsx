803cb670c5224590c98721e869fc21fae394d98e
Anthony Greene <agreene@sidlee.com>
2022-07-03 16:53:09 -0400

    add PortableText

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -38,7 +38,30 @@ function Post({ post }: Props) {
           </p>
         </div>
         <div className='mt-10'>
-
+         
+          <PortableText
+            className=''
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className='text-2xl font-bold my-5' {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className='text-xl font-bold my-5' {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className='ml-4 list-disc'>{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href='' className='text-blue-500 hover:underline'>
+                  {children}
+                </a>
+              ),
+            }}
+          />
+     
         </div>
       </article>
     </main>
